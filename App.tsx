a01cf9479149a202ba2cfcd14f7452b7a67d62ac
Mazhar Fareed <mazhar.fareed966@gmail.com>
2023-07-12 14:33:18 +0500

    New Text Field Added

--- App.tsx
+++ App.tsx
@@ -1,10 +1,11 @@
-import {View, Text} from 'react-native';
+import {View, Text, TextInput} from 'react-native';
 import React from 'react';
 
 const App = () => {
   return (
     <View style={{backgroundColor: 'red'}}>
       <Text>App67</Text>
+      <TextInput style={{borderRadius: 1, backgroundColor: 'red'}} />
     </View>
   );
 };
