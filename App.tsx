ed4d178fa164a293ee19d88ca84c3b885f526f8d
Mazhar Fareed <mazhar.fareed966@gmail.com>
2023-07-12 15:01:53 +0500

    Error Resolved

--- App.tsx
+++ App.tsx
@@ -1,9 +1,11 @@
-import {View, Text, TextInput} from 'react-native';
+import {View, Text, Button} from 'react-native';
 import React from 'react';
 
 const App = () => {
   return (
     <View style={{backgroundColor: 'red'}}>
+      <Text>App67987</Text>
+      <Button title="NewButton" color={'Puink'} />
       <Text>App67</Text>
       <TextInput style={{borderRadius: 1, backgroundColor: 'red'}} />
     </View>
