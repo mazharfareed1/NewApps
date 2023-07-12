a48070b41e79a08f22ed406d71f7fc91b01e626b
Mazhar Fareed <mazhar.fareed966@gmail.com>
2023-07-12 14:29:37 +0500

    New Button Added

--- App.tsx
+++ App.tsx
@@ -1,10 +1,11 @@
-import {View, Text} from 'react-native';
+import {View, Text, Button} from 'react-native';
 import React from 'react';
 
 const App = () => {
   return (
     <View style={{backgroundColor: 'red'}}>
-      <Text>App67</Text>
+      <Text>App67987</Text>
+      <Button title="NewButton" color={'Puink'} />
     </View>
   );
 };
